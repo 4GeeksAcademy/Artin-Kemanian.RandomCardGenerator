71255ec84d127575833815fda8b25dce48c769df
Artin-Kemanian <126287016+Artin-Kemanian@users.noreply.github.com>
2023-04-24 20:06:37 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".card").innerHTML = randomNumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numbersIndex = Math.floor(Math.random() * numbers.length);
+
+  return numbers[numbersIndex];
+};
+
+let randomSuit = () => {
+  let suits = ["spade", "club", "heart", "diamond"];
+  let suitIndex = Math.floor(Math.random() * suits.length);
+
+  return suits[suitIndex];
 };
